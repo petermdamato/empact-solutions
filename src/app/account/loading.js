355c5b1b12bb50a7e560e8f0f2da963c5be09b07
Peter DAmato <peterdamato@Mac.lan>
2025-10-12 20:25:30 -0400

    Fix characters

---
+++ src/app/account/loading.js
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div>Loading...</div>
+    </div>
+  );
+}
